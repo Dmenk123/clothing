f96a0934becde80f80f7d928747bd2e12a928b0b
Dmenk123 <rizkiyuandaa@gmail.com>
2021-05-10 11:31:06 +0700

    yahya

--- application/modules/homepage/views/view_homepage_new_produk.php
+++ application/modules/homepage/views/view_homepage_new_produk.php
@@ -22,14 +22,10 @@ $query = $this->db->get(''); ?>
 
 	<div class="box">
 		<div class="container">
-			<div class="row col-md-12" style="display:flex;flex-wrap:wrap;">
-				<?php foreach ($query->result() as $key => $val) {
-					if (($key + 0) % 2 == 0) {
-						echo '<div class="row col-md-12" style="display:flex;flex-wrap:wrap;">';
-					}
-				?>
-					<div class="col-md-6" style="width: calc(50% - 1rem);">
-					<!-- <div class="col-md-6"> -->
+
+			<div class="col-sm-12 row" style="display:flex;flex-wrap:wrap;">
+				<?php foreach ($query->result() as $key => $val) { ?>
+					<div class="col-sm-6" style="width: calc(50% - 1rem);">
 						<div class="card">
 							<!-- <div class="card-head">
 								<img src="<?php echo config_item('assets'); ?>img/produk/<?php echo $val->nama_gambar; ?>" alt="" class="img-responsive2">
@@ -56,44 +52,37 @@ $query = $this->db->get(''); ?>
 								</div>
 								<div class="product-properties">
 									<span class="product-size">
-										<h4><b>Rp. 350.000<b></h4>
+										<h4><b>Rp. 350.000</b></h4>
 										<!-- <ul class="ul-size">
-												<li><a href=""></a>7</li>
-												<li><a href=""></a>8</li>
-												<li><a href=""></a>9</li>
-												<li><a href="" class="active"></a>10</li>
-												<li><a href=""></a>11</li>
-											</ul> -->
+											<li><a href=""></a>7</li>
+											<li><a href=""></a>8</li>
+											<li><a href=""></a>9</li>
+											<li><a href="" class="active"></a>10</li>
+											<li><a href=""></a>11</li>
+										</ul> -->
 									</span>
 									<!-- <span class="product-color">
-											<h4>Colors</h4>
-											<ul class="ul-color">
-												<li><a href="" class="orange"></a></li>
-												<li><a href="" class="green"></a></li>
-												<li><a href="" class="yellow"></a></li>
-											</ul>
-											</span> -->
+										<h4>Colors</h4>
+										<ul class="ul-color">
+											<li><a href="" class="orange"></a></li>
+											<li><a href="" class="green"></a></li>
+											<li><a href="" class="yellow"></a></li>
+										</ul>
+									</span> -->
 									<br>
 									<!-- <span class="product-price">
 										<b>Beli Sekarang</b>
 									</span> -->
 								</div>
 							</div>
-						</diV>
+						</div>
+
 					</div>
-
-					<?php
-					if (($key + 1) % 2 == 0) {
-						echo '</div>';
-					}
-					?>
 				<?php } ?>
-
 			</div>
-
-			<!-- </div> -->
 		</div>
 
 
 		<!-- /.container -->
 	</div>
+</div>
