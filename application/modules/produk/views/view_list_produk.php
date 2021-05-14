7088a829d53df1db3c8bae3ebe805f79c469a647
nurchyn <nurcahyono320@gmail.com>
2021-05-14 10:15:44 +0700

    asbd

--- application/modules/produk/views/view_list_produk.php
+++ application/modules/produk/views/view_list_produk.php
@@ -66,28 +66,134 @@
         <!-- *** END BOX INFO BAR *** -->
 
         <!-- *** ROW PRODUCT *** -->
-        <div class="row products">
-            <?php foreach ($results as $val) { ?>
-            <div class="col-md-4 col-sm-6">
-                <div class="product">
-                    <a href="<?php echo site_url('produk/produk_detail/').$val->slug; ?>">
-                        <?php $link_img = $val->nama_gambar; ?>
-                            <img src="<?php echo site_url('assets/img/produk/'.$link_img.''); ?>" alt="" class="img-responsive">
-                    </a>
-                    <div class="text">
-                        <p style="text-align: center; font-size: 16px;"><a href="<?php echo site_url('produk/produk_detail/').$val->slug; ?>" style="color: black;"><?php echo $val->nama_produk; ?></a></p>
-                        <p class="price"><strong>Rp. <?php echo number_format($val->harga,0,",","."); ?></strong></p>
-                        <!-- <p class="buttons">
-                            <a href="<?php echo site_url('produk/produk_detail/').$val->slug; ?>" class="btn btn-primary">Beli Sekarang</a>
-                        </p> -->
-                    </div>
-                    <!-- /.text -->
-                </div>
-                <!-- /.product -->
-            </div>
-            <?php } ?>
-            <!-- /.col -->
+        <div class="container" id="tampilan-pc">
+        <div class="col-md-12 row products">
+				<?php foreach ($results as $val) { ?>
+					<div class="col-md-3">
+						<div class="card">
+							<div class="card-head">
+								<a href="<?php echo site_url('produk/produk_detail/').$val->slug; ?>">
+									<img src="<?php echo config_item('assets'); ?>img/produk/<?php echo $val->nama_gambar; ?>" alt="" class="img-responsive2">
+								</a>
+
+							</div>
+							<div class="card-body">
+								<div class="product-desc">
+									<span class="product-title">
+										<b><?php echo $val->nama_produk; ?></b>
+										<!-- <span class="badge">
+											New
+										</span> -->
+									</span>
+									<span class="product-caption">
+										Basket ball collection
+									</span>
+									<span class="product-rating">
+										<i class="fa fa-star yellow"></i>
+										<i class="fa fa-star yellow"></i>
+										<i class="fa fa-star yellow"></i>
+										<i class="fa fa-star yellow"></i>
+										<i class="fa fa-star grey"></i>
+									</span>
+								</div>
+								<div class="product-properties">
+									<span class="product-size">
+										<h4><b><?php echo 'Rp '.number_format($val->harga,0,",","."); ?></b></h4>
+										<!-- <ul class="ul-size">
+											<li><a href=""></a>7</li>
+											<li><a href=""></a>8</li>
+											<li><a href=""></a>9</li>
+											<li><a href="" class="active"></a>10</li>
+											<li><a href=""></a>11</li>
+										</ul> -->
+									</span>
+									<!-- <span class="product-color">
+										<h4>Colors</h4>
+										<ul class="ul-color">
+											<li><a href="" class="orange"></a></li>
+											<li><a href="" class="green"></a></li>
+											<li><a href="" class="yellow"></a></li>
+										</ul>
+									</span> -->
+									<br>
+									<span class="product-price">
+										<a href="<?php echo site_url('produk/produk_detail/').$val->slug; ?>">
+											<b>Beli Sekarang</b>
+										</a>
+									</span>
+								</div>
+							</div>
+						</div>
+
+					</div>
+				<?php } ?>
+		</div>
         </div>
+
+        <div class="container" id="tampilan-hp" style="display:none;padding-left:0px!important;padding-right:0px!important;">
+		
+			<div class="col-sm-12 row products" style="display:flex;flex-wrap:wrap;padding-left:0px!important;padding-right:0px!important;">
+				<?php foreach ($results as $val) { ?>
+					<div class="col-sm-6" style="width: calc(50% - 1rem);">
+						<div class="card">
+							<div class="card-head">
+								<a href="<?php echo site_url('produk/produk_detail/').$val->slug; ?>">
+									<img src="<?php echo config_item('assets'); ?>img/produk/<?php echo $val->nama_gambar; ?>" alt="" class="img-responsive2">
+								</a>
+
+							</div>
+							<div class="card-body">
+								<div class="product-desc">
+									<span class="product-title">
+										<b><?php echo $val->nama_produk; ?></b> 
+										<!-- <span class="badge">
+											New
+										</span> -->
+									</span>
+									<span class="product-caption">
+										Basket ball collection
+									</span>
+									<span class="product-rating">
+										<i class="fa fa-star yellow"></i>
+										<i class="fa fa-star yellow"></i>
+										<i class="fa fa-star yellow"></i>
+										<i class="fa fa-star yellow"></i>
+										<i class="fa fa-star grey"></i>
+									</span>
+								</div>
+								<div class="product-properties">
+									<span class="product-size">
+										<h4><b><?php echo 'Rp '.number_format($val->harga,0,",","."); ?></b></h4>
+										<!-- <ul class="ul-size">
+											<li><a href=""></a>7</li>
+											<li><a href=""></a>8</li>
+											<li><a href=""></a>9</li>
+											<li><a href="" class="active"></a>10</li>
+											<li><a href=""></a>11</li>
+										</ul> -->
+									</span>
+									<!-- <span class="product-color">
+										<h4>Colors</h4>
+										<ul class="ul-color">
+											<li><a href="" class="orange"></a></li>
+											<li><a href="" class="green"></a></li>
+											<li><a href="" class="yellow"></a></li>
+										</ul>
+									</span> -->
+									<br>
+									<span class="product-price">
+										<a href="<?php echo site_url('produk/produk_detail/').$val->slug; ?>">
+											<b>Beli Sekarang</b>
+										</a>
+									</span>
+								</div>
+							</div>
+						</div>
+
+					</div>
+				<?php } ?>
+			</div>
+	</div>
         <!-- /.products -->
 
         <!-- pages -->
